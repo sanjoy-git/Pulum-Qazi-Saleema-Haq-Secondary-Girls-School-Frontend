9e27cfe53f486c4af93098b0e780b8a5476cf336
sanjoylab <sanjoylab66@gmail.com>
2025-10-20 11:09:13 +0600

    Photo and video modal add

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,39 +1,54 @@
+import { Modal, ModalAction, ModalContent } from "keep-react";
+import { ArrowsOut } from 'phosphor-react'
+
 export interface IVideosProps {
-	youtubeVideosUrlsOrIds:[
+	youtubeVideosUrlsOrIds: [
 		{
-			id:{
-				videoId:string;
+			id: {
+				videoId: string;
 			}
 		}
 	] | string[];
-	showVideoNumber:number;
-	moreVideoUrl:string;
+	showVideoNumber: number;
+	moreVideoUrl: string;
 }
 
-export default function Videos ({youtubeVideosUrlsOrIds,showVideoNumber,moreVideoUrl}: IVideosProps) {
+export default function Videos({ youtubeVideosUrlsOrIds, showVideoNumber, moreVideoUrl }: IVideosProps) {
 	return (
 		<div className=' grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3'>
-				{
-					youtubeVideosUrlsOrIds?.filter((item,index:number)=>{
-						if(item){
-							return index<showVideoNumber;
-						}
-					}).map((item: any, index: number) => {
-						if(!item?.id){
-							var youtubeId = item?.split('v=')[1] || item?.split('/shorts/')[1];
-						}
-						return (
-							<div key={index}>
-								{
-									<iframe className=' w-full aspect-video rounded-md' src={`https://www.youtube.com/embed/${item?.id?.videoId?item.id.videoId:youtubeId}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
-								}
-							</div>
-						)
-					})
-				}
+			{
+				youtubeVideosUrlsOrIds?.filter((item, index: number) => {
+					if (item) {
+						return index < showVideoNumber;
+					}
+				}).map((item: any, index: number) => {
+					if (!item?.id) {
+						var youtubeId = item?.split('v=')[1] || item?.split('/shorts/')[1];
+					}
+					return (
+						<div key={index}>
+							{
+								<Modal>
+									<ModalAction asChild>
+										<div>
+											<iframe className=' w-full aspect-video rounded-md' src={`https://www.youtube.com/embed/${item?.id?.videoId ? item.id.videoId : youtubeId}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+											<i className="cursor-pointer">
+												<ArrowsOut/>
+											</i>
+										</div>
+									</ModalAction>
+									<ModalContent className="max-w-[40rem] lg:max-w-[50rem] m-1.5">
+										<iframe className=' w-full aspect-video rounded-md' src={`https://www.youtube.com/embed/${item?.id?.videoId ? item.id.videoId : youtubeId}`} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"></iframe>
+									</ModalContent>
+								</Modal>
+							}
+						</div>
+					)
+				})
+			}
 
-{
-				moreVideoUrl && 
+			{
+				moreVideoUrl &&
 				<div className=" min-h-20 ring-1 ring-teal-600 rounded-md flex justify-center items-center">
 					<a className=" ring-1 ring-teal-500 hover:bg-teal-500 text-center py-1 rounded-md px-3 text-xl " href={moreVideoUrl} target="_blank" rel="noopener noreferrer">More..</a>
 				</div>
--- src/components/photos.tsx
+++ src/components/photos.tsx
@@ -1,19 +1,32 @@
+import { Modal, ModalAction, ModalContent } from "keep-react";
+
 export interface IPhotosProps {
 	photosUrlsOrDriveFileIds: string[];
-	morePhotoUrl:string;
+	morePhotoUrl: string;
 }
 
-export default function Photos({ photosUrlsOrDriveFileIds,morePhotoUrl }: IPhotosProps) {
+export default function Photos({ photosUrlsOrDriveFileIds, morePhotoUrl }: IPhotosProps) {
 	return (
 		<div className=' grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-3'>
 			{
 				photosUrlsOrDriveFileIds?.map((item: any, index: number) => {
-					return <img key={index} className=' rounded-md w-full' src={item} alt="" />
+					return (
+						<div key={index}>
+							<Modal>
+								<ModalAction asChild>
+									<img className=' rounded-md w-full' src={item} alt="" />
+								</ModalAction>
+								<ModalContent className="max-w-[40rem] lg:max-w-[50rem] m-1.5">
+									<img className=' rounded-md w-full' src={item} alt="" />
+								</ModalContent>
+							</Modal>
+						</div>
+					)
 				})
 			}
 
 			{
-				morePhotoUrl && 
+				morePhotoUrl &&
 				<div className=" min-h-32 ring-1 ring-teal-600 rounded-md flex justify-center items-center">
 					<a className=" ring-1 ring-teal-500 hover:bg-teal-500 text-center py-1 rounded-md px-3 text-xl " href={morePhotoUrl} target="_blank" rel="noopener noreferrer">More..</a>
 				</div>
